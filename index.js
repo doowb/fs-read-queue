f46b2fd9a0be517ca224ab85272b82040dc2b94b
Brian Woodward <brian.woodward@gmail.com>
2015-07-01 00:04:21 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * fs-read-queue <https://github.com/doowb/fs-read-queue>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
